a99960fb0398b11af9fbd36eeecc97f9a9e7e813
Erick Garcia meneses <erickarmandogarcia@hotmail.com>
2023-05-28 18:39:50 -0500

    setup config for migration

---
+++ config/config.js
@@ -0,0 +1,13 @@
+import '../dotenv.js'
+
+export default {
+  development: {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.SERVER_HOST,
+    "dialect": process.env.DIALECT
+  },
+  // Add other environments like test and production if needed
+};
+
